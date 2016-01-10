a2f207281c3a363a60fa64ef8efe75cecb5aad2c
Trevor Pierce <trevor.pierce@continuumdesign.net>
2016-01-10 10:56:42 -0600

    Adding Handlebars partials for separation of concerns

--- build.js
+++ build.js
@@ -1,7 +1,14 @@
 var Metalsmith = require('metalsmith'),
     markdown = require('metalsmith-markdown'),
-    templates = require('metalsmith-templates');
+    templates = require('metalsmith-templates'),
+    Handlebars = require('handlebars'),
+    fs = require('fs');
 
+// Add Handlebars partials
+Handlebars.registerPartial('header', fs.readFileSync(__dirname + '/templates/partials/header.hbt').toString());
+Handlebars.registerPartial('footer', fs.readFileSync(__dirname + '/templates/partials/footer.hbt').toString());
+
+// Metalsmith build plugins
 Metalsmith(__dirname)
     .source('src')
     .use(markdown({
